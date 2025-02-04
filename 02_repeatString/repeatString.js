128a5c688f417e9613aa7e9e250ae20ca3c7548d
Daniel <rasay_d@yahoo.com>
2025-02-04 14:18:06 +0800

    Finish exercise 1 & 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let strRepeat = "";
+    if (num !== +num || num < 0) {
+        return `ERROR`
+    // if (num == NaN) return `Please input a valid number in 2nd argument.`;
+    } else 
+        for (i= num;i>0;--i) {
+            strRepeat += string;}
+    return strRepeat;
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
