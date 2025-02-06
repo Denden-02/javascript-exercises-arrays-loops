4918e39f4d05361bec73ac9adfb553e3a46921e9
Daniel <rasay_d@yahoo.com>
2025-02-06 18:29:22 +0800

    Finish 03 & 04 reverseStr & removeFromArr

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(Str) {
+    let revString = '';
+    // use short-circuit conditionals
+    if (!Str && Str !== '') {
+        return 'Please input a valid argument';
+    } 
+    Str = Str.toString();  
+    for (let i=-1;i >= -Str.length; --i) revString += Str.at(i);
+    return revString;
+    // or use return Str.split("").reverse("").join("") which uses ARRAY to split the string into parts, reverse the array, and concat array elements using join()
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,32 @@
-const removeFromArray = function() {
-};
+// create new array > prompts for elements with array length 4 
+// > creates a function that removes elements in an array
+        // create a new Array() which includes only the 'not removed' parts using the FOR loop
+
+// let lst = new Array();
+// let new_lst = function() {
+//     for (let i=0;i<4;++i) {
+//         lst.push(prompt("Enter array values:"));
+//     }
+// }
+
+
+
+const removeFromArray = function(lst, first, second, third, fourth) {
+    var arrayAdd = [];
+    for (let item of lst) {
+        if (item !== first && item !== second && item !== third && item !== fourth) {
+            arrayAdd.push(item);
+        }; 
+    }
+    return arrayAdd;
+}
+
+// removeFromArray([1, 2, 3, 4], 3, 2)
+// // new_lst();
+// console.log(`Length of lst array: ${lst.length} - ${lst}`);
+// console.log(removeFromArray(lst, 1,2,3,4));
+// console.log(arrayAdd);
+ 
 
 // Do not edit below this line
 module.exports = removeFromArray;
