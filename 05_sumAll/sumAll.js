ba409cb7cebe8c1d47420b686ef3689651c22310
Daniel <rasay_d@yahoo.com>
2025-02-12 15:25:43 +0800

    Finish exercise 05 & 06

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,28 @@
-const sumAll = function() {
+// if NaN, float, negative numbers, & strings, result is 'ERROR'
+// create let variables for the two arguments (must be integers)
+// Large number comes last in argument (use IF)
+const sumAll = function(a, b) {
+    if (Number.isInteger(a) && Number.isInteger(b)) {
+        if (a < 0 || b < 0) {
+            return 'ERROR';
+        } else if (a <= b) {
+            let smallerNum = a;
+            let biggerNum = b;
+            let sumNum = 0;
+            for (; a <= b ; ++a) {
+                sumNum += a;
+            }
+            return sumNum;
+        } else if (a >= b) {
+            let smallerNum = b;
+            let buggerNum = a;
+            let sumNum = 0;
+            for (; b <= a ; ++b) {
+                sumNum += b;
+            }
+            return sumNum;
+        }
+    } else return 'ERROR'
 
 };
 
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,22 @@
-const leapYears = function() {
+//getYear value, first check if number is integer and 4 in length
+// check for the 3 conditionals for leap year: - getYear > 99 && getYear % 4 == 0 && !getYear % 100 == 0 && getYear % 400 == 0;
+// return true if goods
 
+const leapYears = function(getYear) {
+    // while (true) {
+    //     getYear = +(prompt("Input year: "));
+    //     if (!Number.isInteger(getYear)) {
+    //         alert('Please enter a valid year');
+    //             continue;
+    //     } else break;
+    // };
+    if (getYear > 99 && getYear % 4 == 0 && !(getYear % 100 == 0)) {
+        return true;
+    } else if (getYear % 100 == 0 && getYear % 400 == 0) {
+        return true;
+    } else return false;
 };
 
 // Do not edit below this line
+
 module.exports = leapYears;
